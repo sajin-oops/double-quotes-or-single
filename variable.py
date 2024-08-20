9e7ab76440faabb19e20e9577660ea6a127e6bf2
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-20 20:21:04 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,36 @@
+'''
+Casting
+If you want to specify the data type of a variable, this can be done with casting.\\
+'''
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+
+print(x,y,z)
+
+# O/P (type)- <class 'str'> <class 'int'> <class 'float'>
+# - 3 3 3.0
+a = 4
+print(type(a))
+#O/P - <class 'int'>
+
+
+name = "Sajin"
+n = 30
+
+print(type(name))
+print(type(n))
+
+#O/P 
+'''
+<class 'str'>
+<class 'int'>
+'''
+#double quotes are the same as single quotes:
+
+a = "SAJIN"
+b = 'AARON'
+print(a)
+print(b)
